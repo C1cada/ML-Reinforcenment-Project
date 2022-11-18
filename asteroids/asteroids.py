42a6968a6edfc9bb96549c1a35fa3ecc5d9a20b3
C1cada <Gabrielhwgo1@gmail.com>
2022-11-18 12:42:47 -0500

    comments

--- asteroids/asteroids.py
+++ asteroids/asteroids.py
@@ -25,11 +25,12 @@ import gym
 import numpy as np
 
 def preprocess(I):
-    I = I[35:195]
-    I = I[::2,::2,0]
-    I[I == 112] = 0
-    I[I != 0] = 1
-    return I.astype(np.float).ravel()
+    """make the data returned by gym more usable"""
+    I = I[35:195] # cut off some of the sides to make it square
+    I = I[::2,::2,0] # compact by skipping every other and using shorthand for color
+    I[I == 112] = 0 #ignore the score letters
+    I[I != 0] = 1 #set everything other than the score and backround to 1
+    return I.astype(np.float).ravel() # return the 2d list as a 1d list of floats
 
 env = gym.make("ALE/Asteroids-v5",frameskip=3, render_mode='human',)
 observation = env.reset()
@@ -41,19 +42,21 @@ episode_number = 0
 while True:
     action = 0
     observation, reward, done, info = env.step(action)
-    observation2, reward2, done, info = env.step(action)
+    observation2, reward2, done, info = env.step(action) #need to steps due to the alternating states that are returned
     reward = reward + reward2
     state = preprocess(observation + observation2)
     difference = state - prev_state if prev_state is not None else np.zeros(D)
     prev_state = state
 
     """
-    
+    Check the policy
+    Record memory
     """
     if done:
         episode_number += 1
         """
-        
+        Adjust policy
+        Discount reward
         """
 
     
