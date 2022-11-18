f44c6677bef8aa45ef381f9d4637efb32f21f3ec
C1cada <Gabrielhwgo1@gmail.com>
2022-11-18 12:36:20 -0500

    data done

--- asteroids/asteroids.py
+++ asteroids/asteroids.py
@@ -23,41 +23,50 @@ RL:
 
 import gym
 import numpy as np
-env = gym.make("ALE/Asteroids-v5",frameskip=3, render_mode='human',)
-observation = env.reset()
-print(type(observation))
-print(observation.shape)
-print(observation[0][0])
-print(env.action_space)
-
-def merge_frames():
-    pass
 
 def preprocess(I):
     I = I[35:195]
     I = I[::2,::2,0]
     I[I == 112] = 0
+    I[I != 0] = 1
+    return I.astype(np.float).ravel()
+
+env = gym.make("ALE/Asteroids-v5",frameskip=3, render_mode='human',)
+observation = env.reset()
+prev_state = None
+D = 80*80
+episode_number = 0
     
 
 while True:
     action = 0
     observation, reward, done, info = env.step(action)
-    observation2, reward2, done2, info2 = env.step(action)
-    # observation = preprocess(observation + observation2)
-    observation = observation + observation2
+    observation2, reward2, done, info = env.step(action)
     reward = reward + reward2
+    state = preprocess(observation + observation2)
+    difference = state - prev_state if prev_state is not None else np.zeros(D)
+    prev_state = state
+
+    """
+    
+    """
+    if done:
+        episode_number += 1
+        """
+        
+        """
 
     
 
     
     # print(observation.shape)
     # observation = observation[35:195]
-    ob = observation[::2,::2, 0]
+    # ob = observation[::2,::2, 0]
     # observation[observation == 144] = 0 #turn bacround colors to 0
     # ob[ob == [214, 214, 214]] = 0
     # ob[ob == [184, 50, 50]] = 0
     # observation[observation == ] = 0 #turn backround colors to 0
     # observation[observation != 0] = 1
-    print(ob[ob != 0])
+    # print(ob[ob != 0])
     # print(info)
     # print(len(ob[ob != 0]))
